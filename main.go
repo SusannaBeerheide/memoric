e87248988796f15340eccea61191e71e55b35a80
Susanna Beerheide <beerheide@hhg-kreuzberg.de>
2023-04-23 11:00:32 +0200

    Neues main.go

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"image/color"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/widget"
+//	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/canvas"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello World")
+	w := a.NewWindow("Rectangle")
 	w.Resize(fyne.NewSize(800, 800))
 
-	w.SetContent(widget.NewLabel("Hello World!"))
+	
+
+	rect := canvas.NewRectangle(color.Black)
+	w.SetContent(rect)
+
+	//w.SetContent(widget.NewLabel("Hello World!"))
 	w.ShowAndRun()
 }
---
+++ main2.go
@@ -0,0 +1,49 @@
+ /*package main
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+)
+
+func main() {
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Grid Layout")
+
+	text1 := canvas.NewText("1", color.Black)
+	text2 := canvas.NewText("2", color.Black)
+	text3 := canvas.NewText("3", color.Black)
+	text4 := canvas.NewText("4", color.Black)
+	text5 := canvas.NewText("5", color.Black)
+	text6 := canvas.NewText("6", color.Black)
+	grid := container.New(layout.NewGridLayout(2), text1, text2, text3, text4, text5, text6)
+	myWindow.SetContent(grid)
+	myWindow.ShowAndRun()
+}
+*/
+package main
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Form Layout")
+
+	label1 := widget.NewLabel("Label 1")
+	value1 := widget.NewLabel("Value das ist jetzt ganz lang und noch länger")
+	label2 := widget.NewLabel("Label 2")
+	value2 := widget.NewLabel("Something")
+	grid := container.New(layout.NewGridLayout(3), label1, value1, label2, value2)
+	myWindow.SetContent(grid)
+	myWindow.ShowAndRun()
+}
+
+
