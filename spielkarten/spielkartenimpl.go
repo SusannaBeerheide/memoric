66fff231b72bf706b9c2920e6e436d98e4057ff6
Susanna Beerheide <beerheide@hhg-kreuzberg.de>
2023-04-30 20:45:40 +0200

    Arbeigen am widget

--- spielkarten/spielkartenimpl.go
+++ spielkarten/spielkartenimpl.go
@@ -5,6 +5,7 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/widget"
 )
 
 type karte struct {
@@ -12,10 +13,35 @@ type karte struct {
 	quadrat   fyne.CanvasObject
 }
 
+// https://github.com/stuartdd2/developer.fyne.io/blob/master/extend/custom-widget.md
+
+type unserWidget struct {
+	widget.BaseWidget
+	text string
+}
+
+type myWidgetRenderer struct {
+	widget     *unserWidget
+	background *canvas.Rectangle
+	text       *canvas.Text
+}
+
+func newWidget() *unserWidget {
+	var w *unserWidget
+
+	w.text = "TestTest"
+	w.ExtendBaseWidget(w)
+	return w
+}
+
+func (w *unserWidget) CreateRenderer() fyne.WidgetRenderer {
+	return BaseRen	return widget.NewSimpleRenderer(canvas.NewText(w.text, color.Black))
+}
+
 func New() *karte {
 	var k *karte
 	k = new(karte)
-	k.quadrat = canvas.NewRectangle(color.RGBA{255, 0, 0, 255})
+	k.quadrat = newWidget()
 	return k
 }
 
